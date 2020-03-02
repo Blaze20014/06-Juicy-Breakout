3a4bcc1d0e7596d9a9fdfa210a3636306b1d05fa
Blaze20014 <koutsokids1@gmail.com>
2020-03-01 22:16:49 -0500

    Revert "finish #1"
    This reverts commit c5dd6e624fea3ef18c8d2f5b62298a0db71eec49.

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -2,81 +2,20 @@ extends RigidBody2D
 
 onready var Game = get_node("/root/Game")
 onready var Starting = get_node("/root/Game/Starting")
-onready var Camera = get_node("/root/Game/Camera")
-
-var _start_size
-var _max_offset = 4
-var trauma_color = Color(1, 1, 1, 1)
-var _decay_rate = 0.8
-var _start_position
-var _trauma = 0.0
-var _rotation = 0
-var _rotation_speed = .05
-var _color = 0.0
-var _color_decay = 1
-var _normal_color
 
 func _ready():
 	contact_monitor = true
 	set_max_contacts_reported(4)
-	_start_position = $ColorRect.rect_position
-	_normal_color = $ColorRect.color
-
-
-func _process(delta):
-	if _trauma > 0:
-		_decay_trauma(delta)
-		_apply_shake()
-	if _color > 0:
-		_decay_color(delta)
-		_apply_color()
-	if _color == 0 and $ColorRect.color != _normal_color:
-		$ColorRect.color = _normal_color
 
 func _physics_process(delta):
 	# Check for collisions
-	
 	var bodies = get_colliding_bodies()
 	for body in bodies:
 		if body.is_in_group("Tiles"):
 			Game.change_score(body.points)
-			_add_color(1.0)
 			body.queue_free()
-			$Hit.playing = true
-			#Camera.add_trauma(_trauma)
-		_add_trauma(2.0)
-		
-		if body.name == "Paddle":
-			$PaddHit.playing = true
+	
 	if position.y > get_viewport().size.y:
 		Game.change_lives(-1)
 		Starting.startCountdown(3)
 		queue_free()
-
-func _add_color(amount):
-	_color += amount
-
-func _apply_color():
-	var a = min(1, _color)
-	$ColorRect.color = _normal_color.linear_interpolate(trauma_color, a)
-
-func _decay_color(delta):
-	var change = _color_decay * delta
-	_color = max(_color - change, 0)
-
-func _add_trauma(amount):
-	_trauma = min(_trauma + amount, 1)
-	
-func _decay_trauma(delta):
-	var change = _decay_rate * delta
-	_trauma = min(_trauma - change, 0)
-
-func _apply_shake():
-	var shake = _trauma * _trauma
-	var o_x = _max_offset * shake * _get_neg_or_pos_scale()
-	var o_y = _max_offset * shake * _get_neg_or_pos_scale()
-	$ColorRect.rect_position = _start_position * Vector2(o_x, o_y)
-	
-
-func _get_neg_or_pos_scale():
-	return rand_range(-1.0, 1.0)
